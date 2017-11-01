2e0cb168316ea2f4e961a0e10a56a6d094bc790b
Michael Wanyoike <micwanyoike@gmail.com>
2017-11-01 14:31:06 +0300

    Added project source code

---
+++ TowerDefenseTutorial/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public float speed = 10f;
+
+    private Transform target;
+    private int waypointIndex;
+
+   void Start()
+    {
+        waypointIndex = 0;
+        target = Waypoints.points[waypointIndex];
+    }
+
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, target.position) > 0.2f)
+        {
+            Vector3 movement = target.position - transform.position;
+            transform.Translate(movement.normalized * speed * Time.deltaTime);
+        } else
+        {
+            GetNextWayPoint();
+        }        
+    }
+
+    void GetNextWayPoint()
+    {
+        if(waypointIndex < Waypoints.points.Length - 1)
+        {
+            waypointIndex += 1;
+            target = Waypoints.points[waypointIndex];
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
