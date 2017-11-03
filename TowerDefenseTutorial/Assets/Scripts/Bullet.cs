38828692d07156a76c8de694fd8d516dccef11e8
Michael Wanyoike <micwanyoike@gmail.com>
2017-11-04 02:02:40 +0300

    Added Firing Mechanism

---
+++ TowerDefenseTutorial/Assets/Scripts/Bullet.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public float speed = 70f;
+    public GameObject bulletParticlesPrefab;
+
+    private Transform target;
+	
+    public void Seek(Transform _target)
+    {
+        target = _target;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 direction = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if (direction.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+        transform.Translate(direction.normalized * distanceThisFrame, Space.World);
+	}
+
+    void HitTarget ()
+    {
+       GameObject bulletParticles =  Instantiate(bulletParticlesPrefab, transform.position, transform.rotation);
+        Destroy(bulletParticles, 2f);
+        Destroy(target.gameObject);
+        Destroy(gameObject);
+    }
+}
--- TowerDefenseTutorial/Assets/Scripts/TurretController.cs
+++ TowerDefenseTutorial/Assets/Scripts/TurretController.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TurretController : MonoBehaviour {
-        
-    public float range = 15f;
-    public float turnSpeed = 10f;
+     
+    [Header("Upgradeable Attributes")]
+    // Target Locking Variables
+    public float range = 15f;    
+    public float fireRate = 1f;
+
+    [Header("Unity Setup Fields")]
     public Transform pivot;
+    public float turnSpeed = 10f;
+    public GameObject bulletPrefab;
+    public Transform firePoint;
 
     private Transform target;
     private string enemyTag = "Enemy";
+
+    // Firing Mechanism Variables
+    
+    private float fireTimer = 0f;
     
     void Start () {
         InvokeRepeating("UpdateTarget", 0f, 0.5f);
@@ -44,7 +55,26 @@ public class TurretController : MonoBehaviour {
             Vector3 rotation = Quaternion.Lerp(pivot.rotation, lookRotation, turnSpeed *  Time.deltaTime).eulerAngles;
             pivot.rotation = Quaternion.Euler(0f, rotation.y, 0f);            
         }
-	}
+
+        if(fireTimer <= 0f && target != null)
+        {
+            Shoot();
+            fireTimer = 1f / fireRate;
+        }
+
+        fireTimer -= Time.deltaTime;
+    }
+
+    void Shoot()
+    {
+        GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+
+        if(bullet != null) {
+            bullet.Seek(target);
+        }
+       
+    }
 
     void OnDrawGizmosSelected()
     {
