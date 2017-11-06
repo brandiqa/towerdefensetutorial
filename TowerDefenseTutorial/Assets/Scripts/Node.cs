47d525ec611c7aa1e81dc5cac393011e1cbb6c22
Michael Wanyoike <micwanyoike@gmail.com>
2017-11-06 17:35:52 +0300

    Node Highlighter

---
+++ TowerDefenseTutorial/Assets/Scripts/Node.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    public Color hoverColor;
+
+    private Renderer rend;
+    private Color defaultColor;    
+
+	void Start () {
+        rend = GetComponent<Renderer>();
+        defaultColor = rend.material.color;
+	}
+
+    void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = defaultColor;
+    }
+
+
+}
